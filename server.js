8ebca228f624cefbe5b46eff2a629764729ba5bd
mira <khawandmirabelle13@gmail.com>
2025-12-06 17:07:49 +0200

    config.yml

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Uncomment if you are using client-side routing for SPA:
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
