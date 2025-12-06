520f9df2eb8142fd3b041c4b127be7c7d0d6bdb9
mira <khawandmirabelle13@gmail.com>
2025-12-06 18:55:31 +0200

    deploy.yml

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 // Listen on the port provided by EB
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
